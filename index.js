7583f9559e28deac3195b043d45c66e50c52a644
Ephraim-9 <155222344+Ephraim-9@users.noreply.github.com>
2025-03-19 07:51:37 +0500

    hash(key)

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 class HashTable {
     constructor(capacity){
         this.buckets = Array.from({length: capacity}, () => []);
-        this.capacity = 16 || capacity;
+        this.capacity = capacity || 16;
         this.size = 0;
     }
+    hash(key) {
+      let hashCode = 0;
+         
+      const primeNumber = 31;
+      for (let i = 0; i < key.length; i++) {
+        hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      }
+   
+      return hashCode % this.capacity;
+    }          
 }   
+
+const myHashTable = new HashTable (10);
+console.log(myHashTable.hash("avacado"))
+console.log(myHashTable.hash("kiwi"))
+console.log(myHashTable.hash("bannana"))
+console.log(myHashTable.hash("apple"))
